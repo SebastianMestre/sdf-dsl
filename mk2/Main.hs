c7cdbf0ed31afef74f6d4c5fa8fcc2f78b59a6ad
SebastianMestre <mestresebastianunr@gmail.com>
2022-06-19 20:44:23 -0300

    factor out applications in Formula

--- mk2/Main.hs
+++ mk2/Main.hs
@@ -14,22 +14,21 @@ data Shape
   | RotatedXyS Float Shape -- rotates in the xy plane
   | RepeatedXS Float Shape -- repeats linearly in the x axis
   | RepeatedXyS Int Shape -- repeats radially in the xy plane
+  deriving Show
+
+data FormulaOp1 = SqrtF | SinF | CosF
+  deriving Show
+
+data FormulaOp2 = MinF | MaxF | AddF | SubF | MulF | DivF | ModF | AtanF
+  deriving Show
 
 data Formula
   = LetF String Formula Formula
-  | MinF Formula Formula
-  | MaxF Formula Formula
-  | AddF Formula Formula
-  | SubF Formula Formula
-  | MulF Formula Formula
-  | DivF Formula Formula
-  | ModF Formula Formula
-  | SqrtF Formula
-  | SinF Formula
-  | CosF Formula
-  | AtanF Formula Formula
+  | App2F FormulaOp2 Formula Formula
+  | App1F FormulaOp1 Formula
   | VarF String
   | ConstF Float
+  deriving Show
 
 data Ssa
   = MaxS Int Int
@@ -45,14 +44,15 @@ data Ssa
   | AtanS Int Int
   | VarS String
   | ConstS Float
+  deriving Show
 
 -- Shape to SDF formula translation
 
 instance Num Formula where
-  x + y    = AddF x y
-  x * y    = MulF x y
-  x - y    = SubF x y
-  negate x = SubF (ConstF 0) x
+  x + y    = App2F AddF x y
+  x * y    = App2F MulF x y
+  x - y    = App2F SubF x y
+  negate x = App2F SubF (ConstF 0) x
   abs         = undefined
   fromInteger = undefined
   signum      = undefined
@@ -61,15 +61,24 @@ vx = VarF "x"
 vy = VarF "y"
 vz = VarF "z"
 
+maxF = App2F MaxF
+minF = App2F MinF
+modF = App2F ModF
+atanF = App2F AtanF
+
+sqrtF = App1F SqrtF
+cosF = App1F CosF
+sinF = App1F SinF
+
 extrude x w = VarF x - clamped
-  where clamped = MaxF (-w') (MinF w' (VarF x))
+  where clamped = maxF (-w') (minF w' (VarF x))
         w'      = ConstF w
 
 pythagoras x y z =
   LetF "x2" (x * x) $
   LetF "y2" (y * y) $
   LetF "z2" (z * z) $
-  SqrtF (VarF "x2" + VarF "y2" + VarF "z2")
+  sqrtF (VarF "x2" + VarF "y2" + VarF "z2")
 
 expand :: Shape -> Formula
 expand PointS = pythagoras vx vy vz
@@ -96,19 +105,19 @@ expand (RotatedXyS angle s) =
       sinAngle = ConstF $ sin (-angle)
 expand (RepeatedXS interval s) =
   LetF "offset" (ConstF (interval / 2)) $
-  LetF "x" (ModF (vx + offset) (ConstF interval) - offset) $
+  LetF "x" (modF (vx + offset) (ConstF interval) - offset) $
   expand s
     where
       offset = VarF "offset"
 expand (UnionS s1 s2) =
-  MinF (expand s1) (expand s2)
+  minF (expand s1) (expand s2)
 expand (RepeatedXyS times s) =
   LetF "offset" (ConstF (interval / 2)) $
-  LetF "angle"  (AtanF vy vx) $
-  LetF "angle'" (ModF (VarF "angle" + offset) (ConstF interval) - offset) $
+  LetF "angle"  (atanF vy vx) $
+  LetF "angle'" (modF (VarF "angle" + offset) (ConstF interval) - offset) $
   LetF "radius" (pythagoras vx vy (ConstF 0)) $
-  LetF "x"      ((VarF "radius") * (CosF (VarF "angle'"))) $
-  LetF "y"      ((VarF "radius") * (SinF (VarF "angle'"))) $
+  LetF "x"      ((VarF "radius") * (cosF (VarF "angle'"))) $
+  LetF "y"      ((VarF "radius") * (sinF (VarF "angle'"))) $
   expand s
     where
       twopi = 6.28318530718
@@ -116,7 +125,7 @@ expand (RepeatedXyS times s) =
       offset = VarF "offset"
 
 
-
+{--
 optimize :: Formula -> Formula
 optimize (LetF v f1 f2) = LetF v (optimize f1) (optimize f2)
 optimize (MinF f1 f2) = MinF (optimize f1) (optimize f2)
@@ -152,6 +161,9 @@ optimize (SinF f1) = SinF (optimize f1)
 optimize (CosF f1) = CosF (optimize f1)
 optimize (ConstF x) = ConstF x
 optimize (VarF x) = VarF x
+--}
+
+optimize = undefined
 
 -- SDF formula to SSA
 
@@ -185,26 +197,26 @@ lower f = runState (go f defaultEnv) defaultState
     go (ConstF x) e = do
       i1 <- addSsa (ConstS x)
       return i1
-    go (SqrtF f) e     = goOp1 f e SqrtS
-    go (SinF f) e      = goOp1 f e SinS
-    go (CosF f) e      = goOp1 f e CosS
-    go (MinF f1 f2) e  = goOp2 f1 f2 e MinS
-    go (MaxF f1 f2) e  = goOp2 f1 f2 e MaxS
-    go (AddF f1 f2) e  = goOp2 f1 f2 e AddS
-    go (SubF f1 f2) e  = goOp2 f1 f2 e SubS
-    go (MulF f1 f2) e  = goOp2 f1 f2 e MulS
-    go (DivF f1 f2) e  = goOp2 f1 f2 e DivS
-    go (ModF f1 f2) e  = goOp2 f1 f2 e ModS
-    go (AtanF f1 f2) e = goOp2 f1 f2 e AtanS
-
-    goOp1 f e op = do
+    go (App1F op f) e = do
       i1 <- go f e
-      addSsa (op i1)
-
-    goOp2 f1 f2 e op = do
+      addSsa (translateOp1 op i1)
+    go (App2F op f1 f2) e = do
       i1 <- go f1 e
       i2 <- go f2 e
-      addSsa (op i1 i2)
+      addSsa (translateOp2 op i1 i2)
+
+    translateOp1 SqrtF = SqrtS
+    translateOp1 SinF = SinS
+    translateOp1 CosF = CosS
+
+    translateOp2 MinF = MinS
+    translateOp2 MaxF = MaxS
+    translateOp2 AddF = AddS
+    translateOp2 SubF = SubS
+    translateOp2 MulF = MulS
+    translateOp2 DivF = DivS
+    translateOp2 ModF = ModS
+    translateOp2 AtanF = AtanS
 
 -- SSA to Javascript
 
@@ -218,6 +230,7 @@ codegen xs = ["const " ++ emitV n ++ " = " ++ emit x ++ ";\n" | (x, n) <- zip xs
     emit (MulS a b) = emitBinop "*" a b
     emit (AddS a b) = emitBinop "+" a b
     emit (SubS a b) = emitBinop "-" a b
+    emit (DivS a b) = emitBinop "/" a b
     emit (ModS a b) = emitF2 "mod" a b
     emit (SqrtS a) = emitF1 "Math.sqrt" a
     emit (SinS a) = emitF1 "Math.sin" a
