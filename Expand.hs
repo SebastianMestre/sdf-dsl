3f1ff538f55ca74759b0a60082266af383941302
SebastianMestre <mestresebastianunr@gmail.com>
2022-07-02 01:35:13 -0300

    Expand: implement x-repetition of shapes
    also, create a few helpers to remove clutter in Expand

--- Expand.hs
+++ Expand.hs
@@ -4,14 +4,30 @@ import Shape
 import Formula
 import Crosscutting
 
-vpos = VarF () "pos"
+varF = VarF ()
+appF = AppF ()
+letF = LetF ()
+prjF = PrjF ()
 
-vecLiteral (x, y, z) = AppF () MkVecF [LitF x, LitF y, LitF z]
+vpos = varF "pos"
 
-matLiteral (vx, vy, vz) = AppF () MkMatF [vecLiteral vx, vecLiteral vy, vecLiteral vz]
+getX = prjF XF
+getY = prjF YF
+getZ = prjF ZF
+
+withX x e =
+  letF VectorF "__old" e $
+  appF MkVecF [x, getY old, getZ old]
+  where old = varF "__old"
+
+modF x y = appF ModF [x, y]
+
+vecLiteral (x, y, z) = appF MkVecF [LitF x, LitF y, LitF z]
+
+matLiteral (vx, vy, vz) = appF MkMatF [vecLiteral vx, vecLiteral vy, vecLiteral vz]
 
 makeRotationMatrix :: Float -> (Float3, Float3, Float3)
-makeRotationMatrix angle = 
+makeRotationMatrix angle =
   ((cosAngle, -sinAngle, 0),
    (sinAngle,  cosAngle, 0),
    (0,         0,        1))
@@ -21,25 +37,29 @@ makeRotationMatrix angle =
 
 negate3 (x, y, z) = (-x, -y, -z)
 
-extrude radii x = v - AppF () ClampF [v, vecLiteral (negate3 radii), vecLiteral radii]
-  where v = VarF () x 
+extrude radii x = v - appF ClampF [v, vecLiteral (negate3 radii), vecLiteral radii]
+  where v = varF x
 
 expand :: Shape -> Form ()
 expand PointS =
-  AppF () LengthF [vpos]
+  appF LengthF [vpos]
 expand (TranslatedS delta s) =
-  LetF () VectorF "pos" (vpos - vecLiteral delta) $
+  letF VectorF "pos" (vpos - vecLiteral delta) $
   expand s
 expand (InflatedS k s) =
   expand s - LitF k
 expand (ExtrudedS radii s) =
-  LetF () VectorF "pos" (extrude radii "pos") $
+  letF VectorF "pos" (extrude radii "pos") $
   expand s
 expand (RotatedXyS angle s) =
-  LetF () VectorF "pos" (matLiteral (makeRotationMatrix angle) * vpos) $
+  letF VectorF "pos" (matLiteral (makeRotationMatrix angle) * vpos) $
   expand s
-expand (RepeatedXS interval s) = expand s -- TODO
+expand (RepeatedXS interval s) =
+  letF ScalarF "offset" (LitF (interval / 2)) $
+  letF VectorF "pos" (withX (modF (getX vpos + offset) (LitF interval) - offset) vpos) $
+  expand s
+  where offset = varF "offset"
 expand (RepeatedXyS times s) = expand s -- TODO
 expand (UnionS s1 s2) =
-  AppF () MinF [expand s1, expand s2]
+  appF MinF [expand s1, expand s2]
 
