9ebe1eb52c3bcc8edbe1c51cd023c946dc544f8d
SebastianMestre <mestresebastianunr@gmail.com>
2022-12-08 22:46:09 -0300

    elimina backend de JS

--- compiler/Codegen.hs
+++ compiler/Codegen.hs
@@ -1,42 +1,10 @@
-module Codegen where
+module Codegen
+  ( emitGlsl
+  ) where
 
 import Ir
 import Crosscutting
 import Data.List
-
-emitJs :: [Tac] -> String
-emitJs cs = concat $ map (++"\n") $ map (uncurry render) $ zip [0..] cs
-  where
-
-  render idx (ConstT x)                 = renderDecl t idx (show x) where t = ScalarF
-  render idx (VarT t x)                 = renderDecl t idx x
-  render idx (AppT t f as)              = renderDecl t idx (renderApp t f as)
-  render idx (PrjT field a)             = renderDecl t idx (renderPrj field a) where t = ScalarF
-
-  renderDecl ty idx expr                = jsCommented (jsConstDecl (renderAtom (TaVar idx)) expr) (show ty)
-  renderAtom (TaVar n)                  = jsIdentifier ("v" ++ show n)
-  renderAtom (TaConst x)                = jsNumberLiteral x
-  renderApp ScalarF SubF [a0, a1]       = jsBinaryOp "-" (renderAtom a0) (renderAtom a1)
-  renderApp ScalarF AddF [a0, a1]       = jsBinaryOp "+" (renderAtom a0) (renderAtom a1)
-  renderApp ScalarF ClampF [a0, a1, s2] = jsCallExpr "Math.min" [renderAtom a1, jsCallExpr "Math.max" [renderAtom a0, renderAtom a1]]
-  renderApp VectorF MkVecF [a0, a1, a2] = jsArrayLiteral [renderAtom a0, renderAtom a1, renderAtom a2]
-  renderApp t f as                      = jsCallExpr (show f) (map renderAtom as)
-  renderPrj field a                     = jsIndexAccess (renderField field) (renderAtom a)
-
-  renderField XF = jsNumberLiteral 0
-  renderField YF = jsNumberLiteral 1
-  renderField ZF = jsNumberLiteral 2
-
-  -- little shallow DSL for javascript syntax
-  jsIndexAccess idx lhs = lhs ++ "[" ++ idx ++ "]"
-  jsFieldAccess field lhs = lhs ++ "." ++ field
-  jsCommented line comment = line ++ "\t\t\t// " ++ comment
-  jsConstDecl x e = concat ["const ", x ," = ", e, ";"]
-  jsCallExpr f as = f ++ "(" ++ (concat $ intersperse "," as) ++ ")"
-  jsBinaryOp op lhs rhs = lhs ++ " " ++ op ++ " " ++ rhs
-  jsArrayLiteral as = "[" ++ (concat $ intersperse "," as) ++ "]"
-  jsNumberLiteral x = show x
-  jsIdentifier x      = x
 
 emitGlsl :: [Tac] -> String
 emitGlsl cs = concat $ map (++"\n") $ map (uncurry go) $ zip [0..] cs
@@ -79,14 +47,38 @@ emitGlsl cs = concat $ map (++"\n") $ map (uncurry go) $ zip [0..] cs
   renderField YF = glIdentifier "y"
   renderField ZF = glIdentifier "z"
 
-  -- DSL Shallow para expresiones GLSL
-  glFieldAccess field lhs = lhs ++ "." ++ field
-  glBinop op lhs rhs = lhs ++ " " ++ op ++ " " ++ rhs
-  glDecl ty name expr = concat [ty, " ", name, " = ", expr, ";"]
-  glCallExpr func args = func ++ "(" ++ (concat $ intersperse ", " $ args) ++ ")"
-  glConstQual ty = "const " ++ ty
-  glFloat = "float"
-  glVec3 = "vec3"
-  glMat3 = "mat3"
-  glIdentifier x = x
-  glFloatLiteral x = show x
+-- DSL Shallow para expresiones GLSL
+type GLExpr = String
+type GLStmt = String
+type GLName = String
+
+glFieldAccess :: GLName -> GLExpr -> GLExpr
+glFieldAccess field lhs = lhs ++ "." ++ field
+
+glBinop :: String -> GLExpr -> GLExpr -> GLExpr
+glBinop op lhs rhs = lhs ++ " " ++ op ++ " " ++ rhs
+
+glDecl :: GLName -> GLName -> GLExpr -> GLStmt
+glDecl ty name expr = concat [ty, " ", name, " = ", expr, ";"]
+
+glCallExpr :: GLName -> [GLExpr] -> GLExpr
+glCallExpr func args = func ++ "(" ++ (concat $ intersperse ", " $ args) ++ ")"
+
+glConstQual :: GLName -> GLName
+glConstQual ty = "const " ++ ty
+
+glFloat :: GLName
+glFloat = "float"
+
+glVec3 :: GLName
+glVec3 = "vec3"
+
+glMat3 :: GLName
+glMat3 = "mat3"
+
+glIdentifier :: String -> GLName
+glIdentifier x = x
+
+glFloatLiteral :: Float -> GLExpr
+glFloatLiteral x = show x
+
