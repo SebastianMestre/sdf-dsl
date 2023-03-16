a0f571718ea20ba6abf51d3031ab3d3e98ec26bd
Sebastián Mestre <36825825+SebastianMestre@users.noreply.github.com>
2023-03-16 13:21:49 -0300

    Crea una monada especifica para Lower

--- compiler/Lower.hs
+++ compiler/Lower.hs
@@ -5,14 +5,37 @@ import Crosscutting
 import qualified VarLookup
 import Ssa
 import Control.Monad.State
+import Control.Monad
 
 type S = ([Ssa], VarId)
 
 type GEnv = VarLookup.Lookup VarId
 
-addSsa :: Ssa -> State S VarId
+data NameResolution a = NR { runNameResolution :: (Genv, S) -> (a, S) }
+
+instance Functor NameResolution where
+  fmap f mx = mx >>= (return . f)
+
+instance Applicative NameResolution where
+  pure = return
+  (<*>) = ap
+
+instance Monad NameResolution where
+  return a = NR $ \(_, s) -> (a, s)
+  ma >>= g = NR $ \(env, s) -> let
+    (a, s') = runNameResolution f (env, s)
+    (b, s'') = runNameResolution (g a) (env, s')
+    in (b, s'')
+
+getState :: NameResolution S
+getState = NR $ \(_, s) -> (s, s)
+
+getEnv :: NameResolution GEnv
+getSnv = NR $ \(env, s) -> (env, s)
+
+addSsa :: Ssa -> NameResolution VarId
 addSsa x = do
-  (xs, n) <- get
+  (xs, n) <- getState
   put (xs ++ [x], n+1)
   return n
 
@@ -21,8 +44,12 @@ makeVar (SsaVar i) = return i
 makeVar (SsaConst x) = addSsa (ConstT x)
 
 lower :: Form TypeF -> [Ssa]
-lower f = fst $ snd $ runState (go defaultEnv f) defaultState
+lower f = ssa
   where
+
+  (lastValue, (ssa, _)) = runNameResolution (go f) defaultState 
+
+  initial = (defaultEnv, defaultState)
 
   defaultEnv :: GEnv
   defaultEnv = VarLookup.extend ("pos", 0) $ VarLookup.empty
@@ -30,22 +57,13 @@ lower f = fst $ snd $ runState (go defaultEnv f) defaultState
   defaultState :: S
   defaultState = ([VarT VectorF "pos"], 1)
 
-  go :: GEnv -> Form TypeF -> State S SsaArg
-  go env (LetF t h x f1 f2) = do
-    a1 <- go env f1
+  go :: Form TypeF -> NameResolution SsaArg
+  go (VarF t v)         = SsaVar <$> VarLookup.get v <$> getEnv
+  go (LitF ty x)        = SsaConst <$> pure x
+  go (AppF t op as)     = SsaVar <$> addSsa (AppT t op <$> mapM go as)
+  go (PrjF _ field e1)  = SsaVar <$> addSsa (PrjT field <$> go e1)
+  go (LetF t h x f1 f2) = do
+    a1 <- go f1
     i1 <- makeVar a1
-    i2 <- go (VarLookup.extend (x, i1) env) f2
+    i2 <- extendEnv (x, i1) $ go f2
     return i2
-  go env (VarF t v) = do
-    let i1 = VarLookup.get v env
-    return $ SsaVar i1
-  go env (LitF ty x) = do
-    return $ SsaConst x
-  go env (AppF t op as) = do
-    is <- mapM (go env) as
-    i <- addSsa (AppT t op is)
-    return $ SsaVar i
-  go env (PrjF _ field e1) = do
-    i1 <- go env e1
-    i <- addSsa (PrjT field i1)
-    return $ SsaVar i
